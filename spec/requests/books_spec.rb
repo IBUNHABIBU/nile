831c00877abf19346652f7ca6a2b18573253cca3
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-07 00:33:03 +0300

    Write the book spec

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -2,7 +2,17 @@ require 'rails_helper'
 
 describe 'Books API', type: :request do 
     it 'returns all book' do 
-        get 'api/v1/books'
+        get '/api/v1/books'
         expect(response).to have_http_status(:success)
+        expect(response.body.size).to eq(2)
+    end
+    it 'returns number of book' do 
+        get '/api/v1/books'
+        expect(response.body.size).to eq(2)
+    end
+
+    it 'returns number of book' do 
+        get '/api/v1/books'
+        expect(JSON.parse(response.body).size).to eq(2)
     end
 end
