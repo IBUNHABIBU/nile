49114a755b29a6f9fcce88785ba4cdb53d5acf16
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-06 23:48:47 +0300

    Add namespace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
 
   private 
-  
-    def not_destroyed
-      render json: {}, status: :unproccessable_entity
+
+    def not_destroyed(e)
+      render json: { errors: e.record.errors }, status: :unproccessable_entity
     end
 end
