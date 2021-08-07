f9a6c7c016c93f546d7634cde98743d4a3689711
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-07 23:24:06 +0300

    Book author association

--- app/representers/books_representer.rb
+++ app/representers/books_representer.rb
@@ -3,7 +3,18 @@ class BooksRepresenter
         @books = books 
     end
 
+    def as_json 
+        books.map do |book|
+            {
+               id: book.id,
+               title: book.title,
+               author_first_name: book.author.first_name, 
+               author_last_name: book.author.last_name,
+            }
+        end
+    end
     private 
 
     attr_reader :books 
+   
 end
