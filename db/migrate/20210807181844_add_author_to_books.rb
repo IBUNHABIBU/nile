5dae874fa41d1315c92d529255580d751ab3e90c
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-07 21:20:50 +0300

    Add author id to book

--- db/migrate/20210807181844_add_author_to_books.rb
+++ db/migrate/20210807181844_add_author_to_books.rb
@@ -1,5 +1,6 @@
 class AddAuthorToBooks < ActiveRecord::Migration[6.1]
   def change
-    add_reference :books, :author, null: false, foreign_key: true
+    # add_reference :books, :author, null: false, foreign_key: true
+    add_reference :books, :author
   end
 end
