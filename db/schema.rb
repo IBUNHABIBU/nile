e5068457eaf98a25f593e41850d7f4e2da5b64a2
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-07 22:38:44 +0300

    Implementing bookrepresenter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_07_182341) do
+ActiveRecord::Schema.define(version: 2021_08_07_184424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2021_08_07_182341) do
     t.string "first_name"
     t.string "last_name"
     t.integer "age"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
