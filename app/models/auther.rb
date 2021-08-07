be6990b4cf69e5fca90e9cafaa814909ea5a165c
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-07 21:33:44 +0300

    Remove author column from books

--- app/models/auther.rb
+++ app/models/auther.rb
@@ -1,2 +1,5 @@
 class Auther < ApplicationRecord
+  validates :first_name, presence: true, length: { minimum: 3}
+  validates :last_name, presence: true, length: { minimum: 3}
+  has_many :books 
 end
