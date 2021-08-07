b1d56e90caebeda208b1df23c5a324061175a914
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-07 21:43:17 +0300

    Remove auther model due to spelling mistakes

--- app/models/auther.rb
+++
@@ -1,5 +0,0 @@
-class Auther < ApplicationRecord
-  validates :first_name, presence: true, length: { minimum: 3}
-  validates :last_name, presence: true, length: { minimum: 3}
-  has_many :books 
-end
--- spec/factories/authers.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :auther do
-    first_name { "MyString" }
-    last_name { "MyString" }
-    age { 1 }
-  end
-end
