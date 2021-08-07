3ff898b0be2336a1144d31c1cb19c18f17bcf576
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-07 17:04:13 +0300

    Test the post request

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,21 +1,27 @@
 require 'rails_helper'
 
 describe 'Books API', type: :request do 
-    it 'returns all book' do 
-        get '/api/v1/books'
-        expect(response).to have_http_status(:success)
-        expect(response.body.size).to eq(2)
-    end
-    it 'returns number of book' do 
-        get '/api/v1/books'
-        expect(response.body.size).to eq(2)
+    describe 'Get /books' do 
+        
+        it 'returns all book' do
+            get '/api/v1/books'
+            expect(response).to have_http_status(:success)
+            expect(response.body.size).to eq(2)
+        end
+        it 'returns number of book' do
+            get '/api/v1/books'
+            expect(response.body.size).to eq(2)
+        end
+        it 'returns number of book' do
+        
+            FactoryBot.create(:book, title: "Jon oke", author: "Majuto")
+            FactoryBot.create(:book, title: "Manjegeka", author: "Imma")
+            get '/api/v1/books'
+            expect(JSON.parse(response.body).size).to eq(2)
+        end
     end
 
-    it 'returns number of book' do 
+    describe 'POST /books' do 
         
-        FactoryBot.create(:book, title: "Jon oke", author: "Majuto")
-        FactoryBot.create(:book, title: "Manjegeka", author: "Imma")
-        get '/api/v1/books'
-        expect(JSON.parse(response.body).size).to eq(2)
     end
 end
