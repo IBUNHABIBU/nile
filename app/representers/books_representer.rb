f17dd731e080818dd9254c1a63a8eaa7f402c563
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-07 23:34:33 +0300

    Concatenate author name

--- app/representers/books_representer.rb
+++ app/representers/books_representer.rb
@@ -8,13 +8,15 @@ class BooksRepresenter
             {
                id: book.id,
                title: book.title,
-               author_first_name: book.author.first_name, 
-               author_last_name: book.author.last_name,
+               author_name: author_name(book)
             }
         end
     end
     private 
 
     attr_reader :books 
-   
+
+    def author_name(book)
+       "#{book.author.first_name} #{book.author.last_name}" 
+    end
 end
