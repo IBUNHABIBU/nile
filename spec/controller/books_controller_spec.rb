e8f12757a0f4082f0038d8f04b60bd563724c3a7
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-30 00:21:01 +0300

    Add controller spec

---
+++ spec/controller/books_controller_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::BooksController, type: :controller do 
+ it 'has max limit of 100 books' do
+  expect(Book).to receive(:limit).with(100).and_call_original
+  get '/api/v1/books', params: { limit: 999} 
+ end
+end
--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -55,10 +55,6 @@ describe 'Books API', type: :request do
             expect(response_body.size).to eq(1)
         end
 
-        it 'has max limit of 100 books' do
-            expect(Book).to receive(:limit).with(100).and_call_original
-            get '/api/v1/books', params: { limit: 999} 
-        end
     end
 
     describe 'POST /books' do 
