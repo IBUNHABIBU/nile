7d5fbee73550158899dfee43f92349f946285212
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-31 13:10:49 +0300

    Add api authentication

--- spec/controller/books_controller_spec.rb
+++ spec/controller/books_controller_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::BooksController, type: :controller do 
- it 'has max limit of 100 books' do
-  expect(Book).to receive(:limit).with(100).and_call_original
-  get :index, params: { limit: 999} 
+ describe 'GET index' do
+  it 'has max limit of 100 books' do
+   expect(Book).to receive(:limit).with(100).and_call_original
+   get :index, params: { limit: 999} 
+  end
  end
+  describe 'POST create' do 
+   let(:book_name) { 'Harry porter' }
+   it 'calls updateSkuJob with correct params' do
+    expect(updateSkuJob).to receive(:perform_later).with(book_name)
+    post :create, params: {
+     author: { first_name: 'jk', last_name: 'rowling', age: 48 },
+     book: { title: 'Harry porter' }
+    } 
+   end
+  end
 end
