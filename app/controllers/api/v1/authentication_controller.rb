a65b7a7d1055e1195a73bde31b8ba717a5935a9c
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-31 14:18:27 +0300

    Test the authentication

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,7 +1,8 @@
 module Api
   module V1
     class AuthenticationController < ApplicationController
-      def create 
+      def create
+       p params.inspect  
       end
     end
   end 
--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 describe 'Authentication', type: :request do 
   describe 'POST /authenticate' do 
    it 'authenticate the client' do
-     post '/api/v1/authenticate' 
+     post '/api/v1/authenticate', params: { username: 'mrmisifa', password: '123'} 
+     expect(response).to have_http_status(:created)
+     expect(response.body).to eq({
+       'token' => '123'
+     })
    end
   end
 end
