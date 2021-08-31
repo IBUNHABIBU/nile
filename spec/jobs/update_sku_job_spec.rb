3dd817e0bb98f789c88fd00484e973da6dd831d6
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-31 12:34:06 +0300

    Test the update sku job

--- spec/jobs/update_sku_job_spec.rb
+++ spec/jobs/update_sku_job_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe UpdateSkuJob, type: :job do
+  let(:book_name) { 'eloquent ruby'}
+  before do
+    allow(Net::HTTP).to receive(:start).and_return(true) 
+  end
   it 'calls sku service with correct params' do 
-    let(:book_name) { 'eloquent ruby'}
-    allow(Net::HTTP).to receive(:start).and_return(true)
     expect_any_instance_of(Net::HTTP::Post).to receive(:body=).with(
       { sku: '123', name: book_name }.to_json
     )
