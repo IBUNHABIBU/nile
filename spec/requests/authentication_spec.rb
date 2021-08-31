836a4600d81855681e7f111d70cd054c37bbb019
IBUNHABIBU <ibunhabib@gmail.com>
2021-08-31 14:40:26 +0300

    Test the authentication

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -5,9 +5,19 @@ describe 'Authentication', type: :request do
    it 'authenticate the client' do
      post '/api/v1/authenticate', params: { username: 'mrmisifa', password: '123'} 
      expect(response).to have_http_status(:created)
-     expect(response.body).to eq({
+     expect(response_body).to eq({
        'token' => '123'
      })
+   end
+
+   it 'returns error when username is missing' do
+    post '/api/v1/authenticate', params: { password: '123'} 
+    expect(response).to have_http_status(:unprocessable_entity)
+   end
+
+   it 'returns error when username is missing' do
+    post '/api/v1/authenticate', params: { username: 'mrmisifa' } 
+    expect(response).to have_http_status(:unprocessable_entity)
    end
   end
 end
