00e9e911ced54359ec0971191ced291c69ed2f9d
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-01 11:22:09 +0300

    Code refactor

--- app/services/authentication_token_service.rb
+++ app/services/authentication_token_service.rb
@@ -1,8 +1,8 @@
 class AuthenticationTokenService
-  hmac_secret = 'my$ecretK3y'
+  HMAC_SECRET = 'my$ecretK3y'
+  ALGORITH_TYPE = 'HS256'
   def self.call
-   hmac_secret = 'my$ecretK3y'
    payload = { "test" => "blah"}
-   JWT.encode payload, hmac_secret, 'HS256'
+   JWT.encode payload, HMAC_SECRET, ALGORITH_TYPE
   end
 end
