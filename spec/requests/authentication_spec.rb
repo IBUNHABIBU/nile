fad4ad698f3e124e44c0dd9aca319d23587b2216
IBUNHABIBU <ibunhabib@gmail.com>
2021-09-01 15:43:01 +0300

    Pass the authentication test

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'Authentication', type: :request do 
   
   describe 'POST /authenticate' do 
-    let(:user) { build(:user) }
+    let(:user) { create(:user) }
    it 'authenticate the client' do
      post '/api/v1/authenticate', params: { username: user.username, password: '123'} 
      expect(response).to have_http_status(:created)
